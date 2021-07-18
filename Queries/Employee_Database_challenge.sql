273a651622bce0ae5283574467ba65e441b77175
Thomas Papiernik <tom.papiernik@gmail.com>
2021-07-18 18:37:40 -0500

    Added mentorship_eligibility.csv

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,20 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count (ut.emp_no) DESC;
 
+-- Deliverable 2, Steps 1-11
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+-- INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = ('9999-01-01')
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
